29def5e6229ed294ee4f2f40bc5577ca0a120891
daniel angel <daniel30081990@gmail.com>
2017-04-29 02:17:45 -0400

    Add course-es.js to Spanish
    Add the updated information of each lesson in Spanish in progress-es.js
    Added attribute slug, desc. Lang, id
    Restructured objects

--- js/course-en.js
+++ js/course-en.js
@@ -13,7 +13,7 @@ var app = new Vue({
             videoOne: {
                 slug: "how-and-where-to-start-part-1",
                 title: 'How and Where to Start - Part 1',
-                under: 'First things first: Before we start entering transactions, we must enter the initial balances for receivables, payables, inventory and cash (including credits)',
+                desc: 'First things first: Before we start entering transactions, we must enter the initial balances for receivables, payables, inventory and cash (including credits)',
                 lang: "en",
                 id: "3oXh3B7fjE8",
                 next: "Dq-WLR00uNg"
@@ -21,7 +21,7 @@ var app = new Vue({
             videoTwo: {
                 slug: "how-and-where-to-start-part-2",
                 title: 'How and Where to Start - Part 2',
-                under: 'We are ready to start: Let’s review how to enter different type of transactions and their impact on the different sections of the application',
+                desc: 'We are ready to start: Let’s review how to enter different type of transactions and their impact on the different sections of the application',
                 lang: "en",
                 id: "Dq-WLR00uNg"
             }
@@ -29,38 +29,53 @@ var app = new Vue({
         invoicing: {
             title: 'Lesson 2: Invoicing',
             videoThree: {
+                slug: "invoice-your-customers-in-a-simple-and-elegant-way",
                 title: 'Invoice your customers in a simple and elegant way',
-                under: 'Create and customize your invoices with your companys logo, print them, or email them directly to your customers.'
+                desc: 'Create and customize your invoices with your companys logo, print them, or email them directly to your customers.',
+                lang: "en",
+                id: "4nTPEaEAx_Q"
             }
 
         },
         accounts: {
             title: 'Lesson 3: Accounts receivable',
             videoFour: {
+                slug: "get-paid-on-time",
                 title: 'Get paid on time',
-                under: 'Keep your sales and collection data up to date to make sure customers are paying you when and how they should.'
+                desc: 'Keep your sales and collection data up to date to make sure customers are paying you when and how they should.',
+                lang: "en",
+                id: "U6PN3F5q-C0"
             }
 
         },
         cashcontrol: {
             title: 'Lesson 4: Cash control',
             videoFive: {
+                slug: "control-your-cash",
                 title: 'Control your cash',
-                under: 'Make sure everything (that should) has been accounted by keeping your cash accounts up to date and reconciled.'
+                desc: 'Make sure everything (that should) has been accounted by keeping your cash accounts up to date and reconciled.',
+                lang: "en",
+                id: "YI5jDEjOq8A"
             }
         },
         cashflow: {
             title: 'Lesson 5: Cash flow management',
             videoSix: {
+                slug: "manage-your-cash-flow",
                 title: 'Manage your cash flow',
-                under: 'Stay ahead of the game and avoid nasty surprises by entering pending collections and payments, as well as recurring expenses.'
+                desc: 'Stay ahead of the game and avoid nasty surprises by entering pending collections and payments, as well as recurring expenses.',
+                lang: "en",
+                id: "dziMNOGtabA"
             }
         },
         inventory: {
             title: 'Lesson 6: Inventory control',
             videoSeven: {
+                slug: "control-your-inventory",
                 title: 'Control your inventory',
-                under: 'Maintain system and real quantities synced, know when to reorder, and see how your products are contributing (or not) to your bottom line.'
+                desc: 'Maintain system and real quantities synced, know when to reorder, and see how your products are contributing (or not) to your bottom line.',
+                lang: "en",
+                id: "bTJ4-rnkzqk"
             }
         }
     }
