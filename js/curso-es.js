01019487e063301d4e3402f829d5445ba9cf01d7
daniel angel <daniel30081990@gmail.com>
2017-05-02 22:57:54 -0400

    Add attribute content section 1 to 15
    Add attribute content section 1 to 15 of videos in progress-es.js

--- js/curso-es.js
+++ js/curso-es.js
@@ -47,18 +47,8 @@ var app = new Vue({
             }
 
         },
-        cashcontrol: {
-            title: 'Lección 4: Control de efectivo',
-            video_1: {
-                slug: "maneje-su-flujo-de-caja",
-                title: "Maneje su flujo de caja",
-                desc: "Manténgase “en la jugada” y evite sorpresas desagradables registrando cobros y pagos pendientes, así como gastos recurrentes.",
-                lang: "es",
-                id: "-z_zb0t9BlE"
-            }
-        },
         cashflow: {
-            title: 'Lección 5: Gestión del flujo de caja',
+            title: 'Lección 4: Manejo de flujo de caja',
             video_1: {
                 slug: "maneje-su-flujo-de-caja",
                 title: "Maneje su flujo de caja",
@@ -67,14 +57,134 @@ var app = new Vue({
                 id: "-z_zb0t9BlE"
             }
         },
-        inventory: {
-            title: 'Lección 6: Control de inventario',
+        revisarpagos: {
+            title: 'Lección 5: Cómo revisar los pagos de ingresos y gastos',
+            video_1: {
+                slug: "maneje-su-flujo-de-caja",
+                title: "Maneje su flujo de caja",
+                desc: "Manténgase ´encima´ de las entrasdas y salidas de efectivo para que su información esté al día y sea confiable .",
+                lang: "es",
+                id: "FVv_pc9AcsU"
+            }
+        },
+        cashcontrol: {
+            title: 'Lección 6: Control de efectivo',
+            video_1: {
+                slug: "maneje-su-flujo-de-caja",
+                title: "Maneje su flujo de caja",
+                desc: "Asegúrese que todo (lo que debería) ha sido contabilizado manteniendo sus cuentas de efectivo actualizadas y reconciliadas.",
+                lang: "es",
+                id: "FVv_pc9AcsU"
+            }
+        },
+        inventory_1: {
+            title: 'Lección 7: Cómo crear impuestos y retenciones - Parte 1',
             video_1: {
                 slug: "controle-su-inventario",
                 title: "Controle su inventario",
-                desc: "Mantenga las unidades físicas sincronizadas con las del sistema, sepa cuándo ordenar sus productos y vea cómo estos contribuyen (o no) a su ganancia.",
+                desc: "Maneje los impuestos y retenciones acorde a las necesidades de su país.",
                 lang: "es",
-                id: "mWFxihAbFZ8"
+                id: "9V0MTOZLcdk"
+            }
+        },
+        inventory_2: {
+            title: 'Lección 8: Cómo crear impuestos y retenciones - Parte 2',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Maneje los impuestos y retenciones acorde a las necesidades de su país.",
+                lang: "es",
+                id: "JAsQTbqEkik"
+            }
+        },
+        creditos: {
+            title: 'Lección 9: Cómo crear notas crédito',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Maneje adecuadamente los créditos dados a clientes y recibidos de proveedores.",
+                lang: "es",
+                id: "IuEDwIhuOEo"
+            }
+        },
+        vencimientos: {
+            title: 'Lección 10: Vencimientos/términos configurables',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Ahora es posible configurar los días usados para calcular el vencimiento en las facturas.",
+                lang: "es",
+                id: "jeXeAUYDjjM"
+            }
+        },
+        precios: {
+            title: 'Lección 11: Más listas de precios',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Maneje hasta cinco listas de precios y actualice fácilmente sus precios usando una plantilla de Excel.",
+                lang: "es",
+                id: "7tyD1wxNM8w"
+            }
+        },
+        item: {
+            title: 'Lección 12: Reportes de ingresos por tipo de ítem',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "El reporte de ingresos por ítem está ahora agrupado por tipo de ítem.",
+                lang: "es",
+                id: "5rxufoEFYoc"
+            }
+        },
+        resumen: {
+            title: 'Lección 13: Exportables en el Resumen',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Ahora es posible exportar datos desde el Resumen, dónde tenemos el Balance general y el Estado de resultados del mes en curso.",
+                lang: "es",
+                id: "as-SxZ1qaXE"
+            }
+        },
+        recurrente: {
+            title: 'Lección 14: Ingresos Recurrentes',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Las transacciones recurrentes ya no están limitadas a gastos. Usted puede ahora crear transacciones de ingresos recurrentes.",
+                lang: "es",
+                id: "Sc0mrIxfwQQ"
+            }
+        },
+        reposicion: {
+            title: 'Lección 15: Determine y use los parámetros para reposición de inventario',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Para determinar los parámetros de reposición de inventario debemos ingresar las cantidades mínimas y máximas que queremos tener para los ítems de inventario.",
+                lang: "es",
+                id: "LUOCE-x8ehM"
+            }
+        },
+        prefijos: {
+            title: 'Lección 15: Determine y use los parámetros para reposición de inventario',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Para usar múltiples prefijos y numeración en nuestras facturas necesitamos cambiar la configuración de la factura.",
+                lang: "es",
+                id: "XT7-2DQYnLI"
+            }
+        },
+        retenciones: {
+            title: 'Lección 15: Use múltiples prefijos en facturas y otros documentos',
+            video_1: {
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Para usar múltiples prefijos y numeración en nuestras facturas necesitamos cambiar la configuración de la factura.",
+                lang: "es",
+                id: "XT7-2DQYnLI"
             }
         }
     }
