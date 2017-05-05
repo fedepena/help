26a29d7fca8cab6db51b5148096d67fedea321de
daniel angel <daniel30081990@gmail.com>
2017-05-04 22:13:39 -0400

    Delete leftover content in .js files,
    Delete leftover content in .js files, leave only up to 6 for as in
    http://www.myabakus.org/help/account-accounting-facil.html
    
    Add correct href link in the contents of the .js files, leave only up
    to the 6th as in
    http://www.myabakus.org/help/currency-accounting-facility.html
    Call from the .js the content of the id attribute on each link, the
    href sluck and correctly place the url

--- js/course-en.js
+++ js/course-en.js
@@ -47,18 +47,8 @@ var app = new Vue({
             }
 
         },
-        inventory: {
-            title: 'Lesson 4: Inventory control',
-            video_1: {
-                slug: "control-your-inventory",
-                title: 'Control your inventory',
-                desc: 'Maintain system and real quantities synced, know when to reorder, and see how your products are contributing (or not) to your bottom line.',
-                lang: "en",
-                id: "bTJ4-rnkzqk"
-            }
-        },
         cashcontrol: {
-            title: 'Lesson 5: Cash control',
+            title: 'Lesson 4: Cash control',
             video_1: {
                 slug: "control-your-cash",
                 title: 'Control your cash',
@@ -68,7 +58,7 @@ var app = new Vue({
             }
         },
         cashflow: {
-            title: 'Lesson 6: Cash flow management ',
+            title: 'Lesson 5: Cash flow management ',
             video_1: {
                 slug: "manage-your-cash-flow",
                 title: 'Manage your cash flow',
@@ -77,114 +67,14 @@ var app = new Vue({
                 id: "dziMNOGtabA"
             }
         },
-        pricelist: {
-            title: 'Lesson 7: More price lists',
+        inventory: {
+            title: 'Lesson 6: Inventory control',
             video_1: {
-                slug: "manage-multiple-price-lists",
-                title: 'Manage multiple price lists',
-                desc: 'Manage up to five price lists and easily update prices using an Excel template.',
+                slug: "control-your-inventory",
+                title: 'Control your inventory',
+                desc: 'Maintain system and real quantities synced, know when to reorder, and see how your products are contributing (or not) to your bottom line.',
                 lang: "en",
-                id: "IVGATE-wngQ"
-            }
-        },
-        dateterms: {
-            title: 'Lesson 8: Configurable due date/terms',
-            video_1: {
-                slug: "configure-the-due-date-terms-of-your-invoices",
-                title: 'Configure the due date\/terms of your invoices',
-                desc: 'It is now possible to configure the days used to calculate due dates in invoices.',
-                lang: "en",
-                id: "7c0z7d4deKs"
-            }
-        },
-        itemtype: {
-            title: 'Lesson 9: Income report by item type',
-            video_1: {
-                slug: "view-your-income-report-by-item-type",
-                title: 'View your income report by item type',
-                desc: 'The income by item report is now grouped by item type.',
-                lang: "en",
-                id: "CWwwtMMq-Jk"
-            }
-        },
-        overview: {
-            title: 'Lesson 10: Exporting from the Overview',
-            video_1: {
-                slug: "export-your-data-from-the-overview",
-                title: 'Export your data from the Overview',
-                desc: 'It is now possible to export data from the Overview, where we have the Balance sheet and Income and expense statement for the current month.',
-                lang: "en",
-                id: "aLjSLv3osT0"
-            }
-        },
-        recurring: {
-            title: 'Lesson 11: Recurring Income',
-            video_1: {
-                slug: "create-recurring-income-transactions",
-                title: 'Create recurring income transactions',
-                desc: 'Recurring transactions are no longer limited to expenses. You can now create recurring income transactions as well.',
-                lang: "en",
-                id: "NyjQTmsx3oI"
-            }
-        },
-        restock: {
-            title: 'Lesson 12: Setup and use the parameters to restock the inventory',
-            video_1: {
-                slug: "setup-and-use-the-parameters-to-restock-the-inventory",
-                title: '',
-                desc: 'To setup the parameters to restock inventory we need to enter the minimum and maximum quantities that we want to have for inventory items.',
-                lang: "en",
-                id: "FA3lPbPeFRA"
-            }
-        },
-        prefixes: {
-            title: 'Lesson 13: Use multiple prefixes on invoices and other documents',
-            video_1: {
-                slug: "use-multiple-prefixes-on-invoices-and-other-documents",
-                title: 'Use multiple prefixes on invoices and other documents',
-                desc: 'To use multiple prefixes and numbering on our invoices we need to change the invoice settings.',
-                lang: "en",
-                id: "slwxy2ZD3s4"
-            }
-        },
-        initinventory: {
-            title: 'Lesson 14: Enter your initial inventory',
-            video_1: {
-                slug: "enter-your-initial-inventory",
-                title: 'Enter your initial inventory',
-                desc: 'To enter the initial inventory we need to go to control inventory and use the add inventory action.',
-                lang: "en",
-                id: "kp2B3y9WKPQ"
-            }
-        },
-        balances: {
-            title: 'Lesson 15: Enter your starting cash balances',
-            video_1: {
-                slug: "enter-your-starting-cash-balances",
-                title: 'Enter your starting cash balances',
-                desc: 'To enter our cash balances we will control - cash.',
-                lang: "en",
-                id: "Ris-dlXSR7o"
-            }
-        },
-        suppliers: {
-            title: 'Lesson 16: Enter your starting balances for customers and suppliers',
-            video_1: {
-                slug: "enter-your-starting-balances-for-customers-and-suppliers",
-                title: 'Enter your starting balances for customers and suppliers',
-                desc: 'To enter all those accounts that we have outstanding receivable and payable we must do it through a transaction.',
-                lang: "en",
-                id: "fSXDJSnE3FY"
-            }
-        },
-        busines: {
-            title: 'Lesson 17: Keep your business finances under control and up to date',
-            video_1: {
-                slug: "keep-your-business-finances-under-control-and-up-to-date",
-                title: 'Keep your business finances under control and up to date',
-                desc: 'Take the financial reins of your business and keep its accounts in order and up to date.',
-                lang: "en",
-                id: "f0dSeD3q3Ns"
+                id: "bTJ4-rnkzqk"
             }
         }
     }
--- js/curso-es.js
+++ js/curso-es.js
@@ -8,9 +8,9 @@ var app = new Vue({
 
         },
         introduction: {
-            title: 'Lección 1: Introducción',
+            title: "Lección 1: Introducción",
             video_1: {
-                slug: "como-y-donde-arrancar-parte-1",
+                slug: 'como-y-donde-arrancar-parte-1',
                 title: 'Cómo y Dónde Arrancar - Parte 1',
                 desc: 'Primero lo primero: Antes de comenzar a ingresar transacciones debemos ingresar los saldos iniciales de cuentas por cobrar, cuentas por pagar, inventario y efectivo (incluyendo créditos)',
                 lang: "es",
@@ -47,8 +47,19 @@ var app = new Vue({
             }
 
         },
+        cuentasbajocontrol: {
+            title: 'Lección 4: Controle su efectivo',
+            video_1: {
+                slug: "controle-su-efectivo",
+                title: "Controle su efectivo",
+                desc: 'Asegúrese que todo (lo que debería) ha sido contabilizado manteniendo sus cuentas de efectivo actualizadas y reconciliadas.',
+                lang: "es",
+                id: "FVv_pc9AcsU"
+            }
+
+        },
         cashflow: {
-            title: 'Lección 4: Maneje su flujo de caja',
+            title: 'Lección 5: Maneje su flujo de caja',
             video_1: {
                 slug: "maneje-su-flujo-de-caja",
                 title: "Maneje su flujo de caja",
@@ -57,152 +68,14 @@ var app = new Vue({
                 id: "-z_zb0t9BlE"
             }
         },
-        revisarpagos: {
-            title: 'Lección 5: Revise los pagos de ingresos y gastos',
+        controlinventario: {
+            title: 'Lección 6: Controle su inventario',
             video_1: {
-                slug: "revise-los-pagos-de-ingresos-y-gastos",
-                title: "Revise los pagos de ingresos y gastos",
-                desc: "Manténgase \"encima\"  de las entradas y salidas de efectivo para que su información esté al día y sea confiable .",
+                slug: "controle-su-inventario",
+                title: "Controle su inventario",
+                desc: "Mantenga las unidades físicas sincronizadas con las del sistema, sepa cuándo ordenar sus productos y vea cómo estos contribuyen (o no) a su ganancia.",
                 lang: "es",
-                id: "pzuHulN2N88"
-            }
-        },
-        cashcontrol: {
-            title: 'Lección 6: Controle su efectivo',
-            video_1: {
-                slug: "controle-su-efectivo",
-                title: "Controle su efectivo",
-                desc: "Asegúrese que todo (lo que debería) ha sido contabilizado manteniendo sus cuentas de efectivo actualizadas y reconciliadas.",
-                lang: "es",
-                id: "FVv_pc9AcsU"
-            }
-        },
-        inventory: {
-            title: 'Lección 7: Cree sus impuestos y retenciones',
-            video_1: {
-                slug: "cree-sus-impuestos-y-retenciones-parte-1",
-                title: "Cree sus impuestos y retenciones - Parte 1",
-                desc: "Maneje los impuestos y retenciones acorde a las necesidades de su país.",
-                lang: "es",
-                id: "9V0MTOZLcdk",
-                next: "JAsQTbqEkik"
-            },
-            video_2: {
-                slug: "cree-sus-impuestos-y-retenciones-parte-2",
-                title: "Cree sus impuestos y retenciones - Parte 2",
-                desc: "Maneje los impuestos y retenciones acorde a las necesidades de su país.",
-                lang: "es",
-                id: "JAsQTbqEkik"
-            }
-        },
-        creditos: {
-            title: 'Lección 8: Cree notas crédito',
-            video_1: {
-                slug: "cree-notas-credito",
-                title: "Cree notas crédito",
-                desc: "Maneje adecuadamente los créditos dados a clientes y recibidos de proveedores.",
-                lang: "es",
-                id: "IuEDwIhuOEo"
-            }
-        },
-        vencimientos: {
-            title: 'Lección 9: Configure los vencimientos/términos de sus facturas',
-            video_1: {
-                slug: "configure-los-vencimientos-terminos-de-sus-facturas",
-                title: "Configure los vencimientos\/términos de sus facturas",
-                desc: "Ahora es posible configurar los días usados para calcular el vencimiento en las facturas.",
-                lang: "es",
-                id: "jeXeAUYDjjM"
-            }
-        },
-        precios: {
-            title: 'Lección 10: Use múltiples listas de precios',
-            video_1: {
-                slug: "use-multiples-listas-de-precios",
-                title: "Use múltiples listas de precios",
-                desc: "Maneje hasta cinco listas de precios y actualice fácilmente sus precios usando una plantilla de Excel.",
-                lang: "es",
-                id: "7tyD1wxNM8w"
-            }
-        },
-        item: {
-            title: 'Lección 11: Vea su reporte de ingresos por tipo de ítem',
-            video_1: {
-                slug: "vea-su-reporte-de-ingresos-por-tipo-de-item",
-                title: "Vea su reporte de ingresos por tipo de ítem",
-                desc: "El reporte de ingresos por ítem está ahora agrupado por tipo de ítem.",
-                lang: "es",
-                id: "5rxufoEFYoc"
-            }
-        },
-        resumen: {
-            title: 'Lección 12: Exporte su información desde el Resumen',
-            video_1: {
-                slug: "exporte-su-informacion-desde-el-resumen",
-                title: "Exporte su información desde el Resumen",
-                desc: "Ahora es posible exportar datos desde el Resumen, dónde tenemos el Balance general y el Estado de resultados del mes en curso.",
-                lang: "es",
-                id: "as-SxZ1qaXE"
-            }
-        },
-        recurrente: {
-            title: 'Lección 13: Cree transacciones de ingresos recurrentes',
-            video_1: {
-                slug: "cree-transacciones-de-ingresos-recurrentes",
-                title: "Cree transacciones de ingresos recurrentes",
-                desc: "Las transacciones recurrentes ya no están limitadas a gastos. Usted puede ahora crear transacciones de ingresos recurrentes.",
-                lang: "es",
-                id: "Sc0mrIxfwQQ"
-            }
-        },
-        reposicion: {
-            title: 'Lección 14: Determine y use los parámetros para reposición de inventario',
-            video_1: {
-                slug: "determine-y-use-los-parametros-para-reposicion-de-inventario",
-                title: "Determine y use los parámetros para reposición de inventario",
-                desc: "Para determinar los parámetros de reposición de inventario debemos ingresar las cantidades mínimas y máximas que queremos tener para los ítems de inventario.",
-                lang: "es",
-                id: "LUOCE-x8ehM"
-            }
-        },
-        prefijos: {
-            title: 'Lección 15: Use múltiples prefijos en facturas y otros documentos',
-            video_1: {
-                slug: "use-multiples-prefijos-en-facturas-y-otros-documentos",
-                title: "Use múltiples prefijos en facturas y otros documentos",
-                desc: "Para usar múltiples prefijos y numeración en nuestras facturas necesitamos cambiar la configuración de la factura.",
-                lang: "es",
-                id: "XT7-2DQYnLI"
-            }
-        },
-        retenciones: {
-            title: 'Lección 16: Ingrese su inventario inicial',
-            video_1: {
-                slug: "ingrese-su-inventario-inicial",
-                title: "Ingrese su inventario inicial",
-                desc: "Para ingresar el inventario inicial necesitamos ir a controlar inventario y usar la acción agregar inventario.",
-                lang: "es",
-                id: "U6kRs9WOFGo"
-            }
-        },
-        saldosiniciales: {
-            title: 'Lección 17: ingrese-sus-saldos-iniciales-de-efectivo',
-            video_1: {
-                slug: "ingrese-sus-saldos-iniciales-de-efectivo",
-                title: "Ingrese sus saldos iniciales de efectivo",
-                desc: "Para ingresar nuestros saldos de efectivo vamos a Controlar - Efectivo.",
-                lang: "es",
-                id: "72cQvGMHuPQ"
-            }
-        },
-        saldosproveedores: {
-            title: 'Lección 18: Ingrese sus saldos iniciales de clientes y proveedores',
-            video_1: {
-                slug: "ingrese-sus-saldos-iniciales-de-clientes-y-proveedores",
-                title: "Ingrese sus saldos iniciales de clientes y proveedores",
-                desc: "Para ingresar todas aquellas cuentas que tenemos pendientes por cobrar y por pagar debemos hacerlo a través de una transacción.",
-                lang: "es",
-                id: "Vnp6ZdoR3BI"
+                id: "mWFxihAbFZ8"
             }
         }
     }
