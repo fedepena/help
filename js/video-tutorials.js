90359ef6d4e7653f659f4ac15a8d98a161e3ede9
daniel angel <daniel30081990@gmail.com>
2017-05-09 00:44:36 -0400

    Add the contents of the videos in a video-tutorials.js file with Vue.js
    Add the contents of the videos in a video-tutorials.js file with Vue.js
    from video-tutorials.html from the 7th onwards, delete video, contents
    from 1 to 6 / call the directives in the html for the content view

--- js/video-tutorials.js
+++ js/video-tutorials.js
@@ -14,65 +14,98 @@ var app = new Vue({
                 desc: 'Manage up to five price lists and easily update prices using an Excel template.',
                 lang: 'en',
                 id: "IVGATE-wngQ"
-            },
-            video_2: {
-                slug: "how-and-where-to-start-part-2",
-                title: 'How and Where to Start - Part 2',
-                desc: 'We are ready to start: Let’s review how to enter different type of transactions and their impact on the different sections of the application',
-                lang: "en",
-                id: "Dq-WLR00uNg"
             }
         },
-        invoicing: {
-            title: 'Lesson 2: Invoicing',
+        terms: {
+            title: 'Configure the due date/terms of your invoices',
             video_1: {
-                slug: "invoice-your-customers-in-a-simple-and-elegant-way",
-                title: 'Invoice your customers in a simple and elegant way',
-                desc: 'Create and customize your invoices with your companys logo, print them, or email them directly to your customers.',
+                slug: "configure-the-due-date-terms-of-your-invoices",
+                title: 'Configure the due date/terms of your invoices',
+                desc: 'It is now possible to configure the days used to calculate due dates in invoices.',
                 lang: "en",
-                id: "4nTPEaEAx_Q"
+                id: "7c0z7d4deKs"
             }
 
         },
-        accounts: {
-            title: 'Lesson 3: Accounts receivable',
+        report: {
+            title: 'View your income report by item type',
             video_1: {
-                slug: "get-paid-on-time",
-                title: 'Get paid on time',
-                desc: 'Keep your sales and collection data up to date to make sure customers are paying you when and how they should.',
+                slug: "view-your-income-report-by-item-type",
+                title: 'View your income report by item type',
+                desc: 'The income by item report is now grouped by item type.',
                 lang: "en",
-                id: "U6PN3F5q-C0"
+                id: "CWwwtMMq-Jk"
             }
 
         },
-        cashcontrol: {
-            title: 'Lesson 4: Cash control',
+        overview: {
+            title: 'Export your data from the Overview',
             video_1: {
-                slug: "control-your-cash",
-                title: 'Control your cash',
-                desc: 'Make sure everything (that should) has been accounted by keeping your cash accounts up to date and reconciled.',
+                slug: "export-your-data-from-the-overview",
+                title: 'Export your data from the Overview',
+                desc: 'It is now possible to export data from the Overview, where we have the Balance sheet and Income and expense statement for the current month.',
                 lang: "en",
-                id: "YI5jDEjOq8A"
+                id: "aLjSLv3osT0"
             }
         },
-        cashflow: {
-            title: 'Lesson 5: Cash flow management ',
+        recurring: {
+            title: 'Create recurring income transactions',
             video_1: {
-                slug: "manage-your-cash-flow",
-                title: 'Manage your cash flow',
-                desc: 'Stay ahead of the game and avoid nasty surprises by entering pending collections and payments, as well as recurring expenses.',
+                slug: "create-recurring-income-transactions",
+                title: 'Create recurring income transactions',
+                desc: 'Recurring transactions are no longer limited to expenses. You can now create recurring income transactions as well.',
                 lang: "en",
-                id: "dziMNOGtabA"
+                id: "NyjQTmsx3oI"
             }
         },
-        inventory: {
-            title: 'Lesson 6: Inventory control',
+        setup: {
+            title: 'Setup and use the parameters to restock the inventory',
             video_1: {
-                slug: "control-your-inventory",
-                title: 'Control your inventory',
-                desc: 'Maintain system and real quantities synced, know when to reorder, and see how your products are contributing (or not) to your bottom line.',
+                slug: "setup-and-use-the-parameters-to-restock-the-inventory",
+                title: 'Setup and use the parameters to restock the inventory',
+                desc: 'To setup the parameters to restock inventory we need to enter the minimum and maximum quantities that we want to have for inventory items.',
                 lang: "en",
-                id: "bTJ4-rnkzqk"
+                id: "FA3lPbPeFRA"
+            }
+        },
+        prefixes: {
+            title: 'Use multiple prefixes on invoices and other documents',
+            video_1: {
+                slug: "use-multiple-prefixes-on-invoices-and-other-documents",
+                title: 'Use multiple prefixes on invoices and other documents',
+                desc: 'To use multiple prefixes and numbering on our invoices we need to change the invoice settings.',
+                lang: "en",
+                id: "slwxy2ZD3s4"
+            }
+        },
+        initialinventory: {
+            title: 'Enter your initial inventory',
+            video_1: {
+                slug: "enter-your-initial-inventory",
+                title: 'Enter your initial inventory',
+                desc: 'To enter the initial inventory we need to go to control inventory and use the add inventory action.',
+                lang: "en",
+                id: "kp2B3y9WKPQ"
+            }
+        },
+        cashbalances: {
+            title: 'Enter your starting cash balances',
+            video_1: {
+                slug: "enter-your-starting-cash-balances",
+                title: 'Enter your starting cash balances',
+                desc: 'To enter our cash balances we will control - cash.',
+                lang: "en",
+                id: "Ris-dlXSR7o"
+            }
+        },
+        customers: {
+            title: 'Enter your starting balances for customers and suppliers',
+            video_1: {
+                slug: "enter-your-starting-balances-for-customers-and-suppliers",
+                title: 'Enter your starting balances for customers and suppliers',
+                desc: 'To enter all those accounts that we have outstanding receivable and payable we must do it through a transaction.',
+                lang: "en",
+                id: "fSXDJSnE3FY"
             }
         }
     }
