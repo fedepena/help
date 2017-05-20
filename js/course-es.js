f9cdcd9afb780a3a37819c37496351b7e77e2de6
daniel angel <daniel30081990@gmail.com>
2017-05-19 20:01:39 -0400

    Add the directives of the new structure in course-es.js
    Add the directives of the new structure in course-es.js for the
    course-accounting-practice.html

--- js/course-es.js
+++ js/course-es.js
@@ -20,8 +20,7 @@ var app = new Vue({
                         lang: "es",
                         id: "61xx5xK9hTM",
                         next: "v5qQ8-xtFEw",
-                        img: "sheet3img sheet-img play-img",
-                        comparador: 1
+                        img: "sheet3img sheet-img play-img"
                     },
                     { //video 1.2
                         slug: "como-y-donde-arrancar-parte-2",
@@ -29,8 +28,7 @@ var app = new Vue({
                         desc: 'Estamos listos para comenzar: Revisemos cómo ingresar diferentes tipos de transacciones y el impacto de las mismas en las diferentes secciones de la aplicación',
                         lang: "es",
                         id: "v5qQ8-xtFEw",
-                        img: "sheet3img sheet-img play-img",
-                        comparador: 1
+                        img: "sheet3img sheet-img play-img"
                     }
                         ]
             },
@@ -43,8 +41,7 @@ var app = new Vue({
                         desc: 'Cree y personalice sus facturas con el logo de su compañía, imprímales o envíelas directamente por email a sus clientes.',
                         lang: "es",
                         id: "bN-rDaysBaY",
-                        img: "play-img play-img-2",
-                        comparador: 2
+                        img: "play-img play-img-2"
                     }
                         ]
             },
@@ -57,8 +54,7 @@ var app = new Vue({
                         desc: 'Mantenga sus cuentas por cobrar al día para asegurarse que sus clientes le están pagando según lo acordado.',
                         lang: "es",
                         id: "K8rIA7hA2Uk",
-                        img: "play-img play-img-3",
-                        comparador: 3
+                        img: "play-img play-img-3"
                     }
                         ]
             },
@@ -72,8 +68,7 @@ var app = new Vue({
                         desc: 'Asegúrese que todo (lo que debería) ha sido contabilizado manteniendo sus cuentas de efectivo actualizadas y reconciliadas.',
                         lang: "es",
                         id: "FVv_pc9AcsU",
-                        img: "play-img play-img-4",
-                        comparador: 4
+                        img: "play-img play-img-4"
                     }
                         ]
             },
@@ -87,8 +82,7 @@ var app = new Vue({
                         desc: "Manténgase “en la jugada” y evite sorpresas desagradables registrando cobros y pagos pendientes, así como gastos recurrentes.",
                         lang: "es",
                         id: "-z_zb0t9BlE",
-                        img: "play-img play-img-5",
-                        comparador: 5
+                        img: "play-img play-img-5"
                     }
                         ]
             },
@@ -102,8 +96,7 @@ var app = new Vue({
                         desc: "Mantenga las unidades físicas sincronizadas con las del sistema, sepa cuándo ordenar sus productos y vea cómo estos contribuyen (o no) a su ganancia.",
                         lang: "es",
                         id: "mWFxihAbFZ8",
-                        img: "play-img play-img-6",
-                        comparador: 6
+                        img: "play-img play-img-6"
                     }
                         ]
             }
