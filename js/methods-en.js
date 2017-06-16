9386a0028a55c7553d921ea12bf4495c06e9fe2c
daniel angel <daniel30081990@gmail.com>
2017-06-15 20:28:45 -0400

    Create methods-es.js and methods-en.js for the slugify function
    Create methods-es.js and methods-en.js for the slugify function that
    generates urls, Separate data from video content tutorials and call it
    from methods-en.js and methods-es.js

---
+++ js/methods-en.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: data,
+    filters: {
+        slugify: function (value) {
+            var minus;
+            minus = value.toLowerCase().replace(/\s+/g, "-");
+            return minus;
+        }
+    }
+});
