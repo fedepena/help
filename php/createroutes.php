698b66ef36286962127b961304c4d50ae3bfa601
daniel angel <daniel30081990@gmail.com>
2017-06-23 00:16:02 -0400

    Add json_decode

--- php/createroutes.php
+++ php/createroutes.php
@@ -5,11 +5,15 @@ $data_en = file_get_contents("../js/videos-en.js", null, null, 10);
 $data_es = file_get_contents("../js/videos-es.js", null, null, 10);
 
 ////creamos el JSON
-$myJSON_en = json_encode($data_en);
-$myJSON_es = json_encode($data_es);
+$myJSONenco_en = json_encode($data_en);
+$myJSONdeco_en = json_decode($myJSONenco_en);
+
+$myJSONenco_es = json_encode($data_es);
+$myJSONdeco_es = json_decode($myJSONenco_es);
+
 $file_en = 'routes_en.json';
 $file_es = 'routes_es.json';
-file_put_contents($file_en, $myJSON_en);
-file_put_contents($file_es, $myJSON_es);
+file_put_contents($file_en, $myJSONdeco_en);
+file_put_contents($file_es, $myJSONdeco_es);
 
 ?>
