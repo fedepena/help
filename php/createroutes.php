765eeb658e67a93d97f8b54749bf69ab43784b92
daniel angel <daniel30081990@gmail.com>
2017-06-24 00:54:56 -0400

    Added code to generate the contents of the array videos-es.js and videos-en.js and create the routes.json

--- php/createroutes.php
+++ php/createroutes.php
@@ -1,19 +1,14 @@
 <?php
     
 //leemos la data
-$data_en = file_get_contents("../js/videos-en.js", null, null, 10);
-$data_es = file_get_contents("../js/videos-es.js", null, null, 10);
+$data_en = file_get_contents("../js/videos-en.js", null, null, 560, 3250 );
+$data_es = file_get_contents("../js/videos-es.js", null, null, 620, 4386);
 
 ////creamos el JSON
-$myJSONenco_en = json_encode($data_en);
-$myJSONdeco_en = json_decode($myJSONenco_en);
+$myJSONenco = json_encode($data_es.",".$data_en);
+$myJSONdeco = json_decode($myJSONenco);
 
-$myJSONenco_es = json_encode($data_es);
-$myJSONdeco_es = json_decode($myJSONenco_es);
-
-$file_en = 'routes_en.json';
-$file_es = 'routes_es.json';
-file_put_contents($file_en, $myJSONdeco_en);
-file_put_contents($file_es, $myJSONdeco_es);
+$file = 'routes.json';
+file_put_contents($file, $myJSONdeco);
 
 ?>
